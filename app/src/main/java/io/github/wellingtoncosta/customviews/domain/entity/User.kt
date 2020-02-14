4fa57112167e6c7afc451abb014739887f2a722a
Wellington Costa <wellington.costa128@gmail.com>
2020-02-14 17:45:40 -0300

    Adding repositories count property.

--- app/src/main/java/io/github/wellingtoncosta/customviews/domain/entity/User.kt
+++ app/src/main/java/io/github/wellingtoncosta/customviews/domain/entity/User.kt
@@ -4,6 +4,7 @@ data class User(
     val userName: String,
     val fullName: String?,
     val avatarUrl: String,
+    val repositories: Int,
     val followers: Int,
     val following: Int,
     val starred: Int
