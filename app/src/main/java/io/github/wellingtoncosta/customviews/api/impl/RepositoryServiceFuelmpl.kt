45e412096215a3a9d2b294d53fe829159eaef9e1
Wellington Costa <wellington.costa128@gmail.com>
2020-02-14 18:15:08 -0300

    Implementing RepositoryService using Fuel http library.

---
+++ app/src/main/java/io/github/wellingtoncosta/customviews/api/impl/RepositoryServiceFuelmpl.kt
@@ -0,0 +1,40 @@
+package io.github.wellingtoncosta.customviews.api.impl
+
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.coroutines.awaitStringResponse
+import io.github.wellingtoncosta.customviews.BuildConfig.API_URL
+import io.github.wellingtoncosta.customviews.api.RepositoryService
+import io.github.wellingtoncosta.customviews.api.response.RepositoryResponse
+import io.github.wellingtoncosta.customviews.api.response.toDomain
+import io.github.wellingtoncosta.customviews.api.util.HTTP_200
+import io.github.wellingtoncosta.customviews.api.util.HTTP_404
+import io.github.wellingtoncosta.customviews.api.util.HTTP_500
+import io.github.wellingtoncosta.customviews.domain.exception.NetworkException
+import io.github.wellingtoncosta.customviews.domain.exception.ServerFailureException
+import io.github.wellingtoncosta.customviews.domain.exception.UserNotFoundException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.list
+
+class RepositoryServiceFuelmpl(
+    private val fuel: Fuel,
+    private val json: Json
+) : RepositoryService {
+
+    private val serializer = RepositoryResponse.serializer().list
+
+    override suspend fun fetchAll(userName: String) = withContext(Dispatchers.IO) {
+        fuel.get("$API_URL/users/$userName/repos")
+            .awaitStringResponse()
+            .run {
+                when (second.statusCode) {
+                    HTTP_200 -> json.parse(serializer, third).map { it.toDomain() }
+                    HTTP_404 -> throw UserNotFoundException(userName)
+                    HTTP_500 -> throw ServerFailureException()
+                    else -> throw NetworkException()
+                }
+            }
+    }
+
+}
