15d30bac4ac1a2e2f7596667c0609d166faa5383
Wellington Costa <wellington.costa128@gmail.com>
2020-02-14 18:49:16 -0300

    Declaring api module and start koin.

--- app/src/main/java/io/github/wellingtoncosta/customviews/App.kt
+++ app/src/main/java/io/github/wellingtoncosta/customviews/App.kt
@@ -1,5 +1,44 @@
 package io.github.wellingtoncosta.customviews
 
 import android.app.Application
+import io.github.wellingtoncosta.customviews.api.RepositoryService
+import io.github.wellingtoncosta.customviews.api.UserService
+import io.github.wellingtoncosta.customviews.api.impl.RepositoryServiceFuelmpl
+import io.github.wellingtoncosta.customviews.api.impl.UserServiceFuelImpl
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.module.Module
+import org.koin.dsl.module
 
-class App : Application()
+open class App : Application() {
+
+    private val apiModule: Module by lazy {
+        module {
+            single { Json(JsonConfiguration.Stable.copy(strictMode = false)) }
+            single<UserService> { UserServiceFuelImpl(json = get()) }
+            single<RepositoryService> { RepositoryServiceFuelmpl(json = get()) }
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin()
+    }
+
+    private fun startKoin() {
+        startKoin {
+            androidLogger()
+
+            androidContext(applicationContext)
+
+            modules(koinModules)
+        }
+    }
+
+    open val koinModules: List<Module> get() = listOf(apiModule)
+
+}
