431b85b84396ceab8dee23a1cc5fe383a75a3797
Wellington Costa <wellington.costa128@gmail.com>
2020-03-06 10:59:06 -0300

    Removing unnecessary Injector object.

--- app/src/main/java/io/github/wellingtoncosta/customviews/Injector.kt
+++
@@ -1,7 +0,0 @@
-package io.github.wellingtoncosta.customviews
-
-import io.github.wellingtoncosta.customviews.di.AppComponent
-
-object Injector {
-    @JvmStatic fun get(): AppComponent = App.get().getComponent()
-}
