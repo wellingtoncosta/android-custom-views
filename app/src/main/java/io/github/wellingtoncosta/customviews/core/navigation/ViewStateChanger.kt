56ca10a37c1f4dca9b3e6bfb897074611ef44ead
Lucas Cavalcante <co.lucasc@gmail.com>
2020-05-09 15:01:25 -0300

    Add ViewStateChanger with coroutines support.

--- app/src/main/java/io/github/wellingtoncosta/customviews/core/navigation/ViewStateChanger.kt
+++ app/src/main/java/io/github/wellingtoncosta/customviews/core/navigation/ViewStateChanger.kt
@@ -10,6 +10,8 @@ import com.zhuinden.simplestack.navigator.changehandlers.FadeViewChangeHandler
 import io.github.wellingtoncosta.customviews.R
 import kotlinx.coroutines.*
 
+const val VIEW_COROUTINE_SCOPE_KEY = R.id.view_coroutine_scope
+
 class ViewStateChanger(
     private val activity: Activity,
     private val root: ViewGroup
@@ -22,7 +24,7 @@ class ViewStateChanger(
             .inflate(newKey.layout(), root, false)
 
         val viewScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
-        newView.setTag(R.id.view_coroutine_scope, viewScope)
+        newView.setTag(VIEW_COROUTINE_SCOPE_KEY, viewScope)
 
         val previousView = root.getChildAt(0)
         Navigator.persistViewToState(previousView)
@@ -34,7 +36,7 @@ class ViewStateChanger(
             return
         }
 
-        val previousViewScope = previousView.getTag(R.id.view_coroutine_scope) as CoroutineScope?
+        val previousViewScope = previousView.getTag(VIEW_COROUTINE_SCOPE_KEY) as CoroutineScope?
         previousViewScope?.cancel()
 
         val viewChangeHandler = when (stateChange.direction) {
